d92da76acdfec1312d805383721996165ed9b200
shanahandnlf <shanahan.danualif@ui.ac.id>
2024-04-17 09:07:14 +0700

    Implement unsubscribe function in Notification controller.

--- src/repository/subscriber.rs
+++ src/repository/subscriber.rs
@@ -1,10 +1,10 @@
-use dashmap::Dashmap;
+use dashmap::DashMap;
 use lazy_static::lazy_static;
 use crate::model::subscriber::Subscriber;
 
 // Singleton of Database
 lazy_static! {
-    static ref SUBSCRIBERS: Dashmap<String, Dashmap<String, Subscriber>> = Dashmap::new();
+    static ref SUBSCRIBERS: DashMap<String, DashMap<String, Subscriber>> = DashMap::new();
 }
 
 pub struct SubscriberRepository;
